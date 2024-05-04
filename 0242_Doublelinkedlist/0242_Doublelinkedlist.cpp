6161559dfe1cb2f5a82e58caafb574e7e605318a
haikal0423 <mhaikal23azka@gmail.com>
2024-05-04 21:09:25 +0700

    Menambhkan cout dan cin

--- 0242_Doublelinkedlist/0242_Doublelinkedlist.cpp
+++ 0242_Doublelinkedlist/0242_Doublelinkedlist.cpp
@@ -15,6 +15,11 @@ Node* START = NULL;
 void addNode()
 {
 	Node* newNode = new Node();
+	cout << "\nEnter the roll number of the student: ";
+	cin >> newNode->noMhs;
+	cout << "\nEnter the name of the student: ";
+	cin >> newNode->name;
+
 
 
 
